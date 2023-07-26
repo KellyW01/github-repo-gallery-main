c45789976c36e58b4bd4172f18674bba8b4e23c5
kelly.w <kelly.conant.winn@gmail.com>
2023-07-26 10:14:31 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const overview = document.querySelector(".overview"); //where profile info will appear
+const username = "KellyW01";
+
+
+
+const profile = async function(){
+    const response = await fetch (`https://api.github.com/users/${username}`);
+    const data = await response.json();
+    console.log ({data});
+    userInfo(data);
+};
+profile ();
+
+const userInfo = function(data){
+    const avatar = data.avatar_url;
+    const name = data.name;
+    const bio = data.bio;
+    const location = data.location;
+    const publicRepos = data.public_repos;
+    
+    const userDiv = document.createElement("div");
+    userDiv.classList.add("user-info");
+    userDiv.innerHTML = 
+    `<figure>
+    <img alt="user avatar" src=${avatar} />
+    </figure>
+    <div>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Bio:</strong> ${bio}</p>
+        <p><strong>Location:</strong> ${location}</p>
+        <p><strong>Number of public repos:</strong> ${publicRepos}</p>
+    </div>`;
+    overview.append(userDiv);
+}
