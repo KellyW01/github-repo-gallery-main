b13f0be59f05782299c1bd7505e9dc7cb44a346a
kelly.w <kelly.conant.winn@gmail.com>
2023-07-31 11:36:22 -0400

    Created functions to display repo Info

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ const displayRepos = function (repos){
         listItem.classList.add("repo"); //give class name to li
         let repoName = eachMethodArray.name; // for each of the method arrays in the repos method collection, grab the key value pair
 
-        listItem.innerHTML = `<h3> ${repoName}</h3>`;
+        listItem.innerHTML = `<h3>${repoName}</h3>`;
         repoList.append(listItem);
         console.log({repoName});
         
@@ -75,8 +75,39 @@ repoList.addEventListener("click", function (e){
 });
 
 const specificRepo = async function(repoName){
-    const specificResponse = await fetch (`https://api.github.com/repos/${username}/${repoName}`);
-    const specificRepoData = await specificResponse.json();
-    console.log({specificRepoData}); 
-    const fetchLanguages = await fetch (``)
+    const fetchInfo = await fetch (`https://api.github.com/repos/${username}/${repoName}`);
+    const repoInfo = await fetchInfo.json();
+    console.log({repoInfo}); 
+    const fetchLanguages = await fetch (repoInfo.languages_url); //don't need to fetch the whole thing again, use dot notation to grab more info
+    const languageData = await fetchLanguages.json();
+    console.log({languageData}); // method(languageData:) key:value CSS:1665 HTML:1905 JavaScript:1271
+    const languages = [];
+        for (let key in languageData){
+            languages.push(key);
+        }
+        console.log({languages});
+    displayRepoInfo(repoInfo, languages);    
+};
+
+const displayRepoInfo = function (repoInfo, languages){
+    //empty the html of the section w/ a class of repo-data where the individual repo data will appear.
+    repoData.innerHTML = '';
+
+    //create a new div element and add the selected repository's name, description, default branch and link to its code on GitHub
+    let newDiv = document.createElement("div");
+    newDiv.innerHTML=
+    `<h3>Name: ${repoInfo.name}</h3>
+    <p>Description: ${repoInfo.description}</p>
+    <p>Default Branch: ${repoInfo.default_branch
+    }</p>
+    <p>Languages: ${languages.join(", ")}</p>
+    <a class="visit" href="${repoInfo.clone_url
+    }" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`;
+    //inside the 5 placeholders, use the json data to grab the properties
+repoData.append(newDiv);
+    //append the new div element to the section with a class of repo-data
+repoData.classList.remove("hide");
+    //(show) the repo data element
+reposSection.classList.add("hide");
+    //hide element with the class of repos
 }
